f737422771c4feed99f93a15ce9e0e6d31d6bc6c
HeejaeJoo <ymount02@gmail.com>
2018-06-29 18:21:01 +0900

    Add a skeleton of wallet status checking.

--- app/javascript/Main.elm
+++ app/javascript/Main.elm
@@ -2,53 +2,83 @@ module Main exposing (..)
 
 import Html exposing (Html, h1, text)
 import Html.Attributes exposing (style)
+import Port
+import Wallet exposing (WalletStatus(NOTFOUND), decodeWalletStatus)
+
 
 -- MODEL
 
+
 type alias Model =
-  {
-  }
+    { walletStatus : WalletStatus }
+
+
 
 -- INIT
 
-init : (Model, Cmd Message)
+
+init : ( Model, Cmd Message )
 init =
-  (Model, Cmd.none)
+    ( { walletStatus = NOTFOUND }, Cmd.none )
+
+
 
 -- VIEW
 
+
 view : Model -> Html Message
 view model =
-  -- The inline style is being used for example purposes in order to keep this example simple and
-  -- avoid loading additional resources. Use a proper stylesheet when building your own app.
-  h1 [style [("display", "flex"), ("justify-content", "center")]]
-     [text "Hello Elm!"]
+    -- The inline style is being used for example purposes in order to keep this example simple and
+    -- avoid loading additional resources. Use a proper stylesheet when building your own app.
+    h1 [ style [ ( "display", "flex" ), ( "justify-content", "center" ) ] ]
+        [ text "Hello Elm!" ]
+
+
 
 -- MESSAGE
 
+
 type Message
-  = None
+    = CheckWalletStatus
+    | UpdateWalletStatus String
+    | None
+
+
 
 -- UPDATE
 
-update : Message -> Model -> (Model, Cmd Message)
+
+update : Message -> Model -> ( Model, Cmd Message )
 update message model =
-  (model, Cmd.none)
+    case message of
+        CheckWalletStatus ->
+            ( model, Port.checkWalletStatus () )
+
+        UpdateWalletStatus str ->
+            ( { model | walletStatus = (decodeWalletStatus str) }, Cmd.none )
+
+        _ ->
+            ( model, Cmd.none )
+
+
 
 -- SUBSCRIPTIONS
 
+
 subscriptions : Model -> Sub Message
 subscriptions model =
-  Sub.none
+    Port.receiveWalletStatus UpdateWalletStatus
+
+
 
 -- MAIN
 
+
 main : Program Never Model Message
 main =
-  Html.program
-    {
-      init = init,
-      view = view,
-      update = update,
-      subscriptions = subscriptions
-    }
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
