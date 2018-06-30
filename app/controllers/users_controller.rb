02aba2593f5b05b0f3a6dcb02afb0343552e8222
Sinhyeok Seo <sinhyeok@chain.partners>
2018-07-01 01:09:52 +0900

    Implement email confirmation logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)    
+    if @user.save
+      UserMailer.email_confirmation(@user).deliver
+      render status: :ok, json: { msg: 'Please confirm your email address to continue' }
+    else
+      render status: :internal_server_error, json: { msg: 'Ooooppss, something went wrong!' }
+    end
+  end
+
   def confirm_email
+    user = User.find_by_confirm_token(params[:id])
+    if user
+      user.email_activate
+      render status: :ok, json: { msg: "Welcome to the EOS! Your email has been confirmed." }
+    else
+      render status: :precondition_failed, json: { msg: 'Sorry. User does not exist' }
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,20 @@
 class User < ApplicationRecord
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true
+  before_create :confirmation_token
 
   def name
     email.split('@').dig(0)
+  end
+
+  def email_activate
+    update!(email_confirmed: true, confirm_token: nil)
+  end
+
+  private
+
+  def confirmation_token
+    if self.confirm_token.blank?
+      self.confirm_token = SecureRandom.urlsafe_base64.to_s
+    end
   end
 end
