f5a6cd0e6e4d5bc59d08a0f8da5beb430144d092
Sinhyeok Seo <sinhyeok@chain.partners>
2018-06-30 23:49:19 +0900

    Remove unnecessary route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'root#index'
-  get 'root/index'
   get 'health_check.html', to: proc{[200, {}, ['<html><head></head><body>HealthCheck OK</body></html>']]}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
