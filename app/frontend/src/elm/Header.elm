047e4838d758c2cd8c5241990ec135ba6445be88
basakkang <redmayne@naver.com>
2018-07-16 17:27:11 +0900

    Add resource informations in Acount type

--- app/frontend/src/elm/Header.elm
+++ app/frontend/src/elm/Header.elm
@@ -5,6 +5,7 @@ import Html.Attributes exposing (placeholder)
 import Html.Events exposing (on, onInput, onClick, keyCode)
 import Http
 import Json.Decode as JD exposing (Decoder)
+import Json.Decode.Pipeline exposing (decode, required, optional)
 import Json.Encode as JE
 
 
@@ -27,15 +28,36 @@ type alias Model =
 
 type alias Account =
     { account_name : String
-    , core_liquid_balance : Maybe String
-    , total_resources : Resource
+    , core_liquid_balance : String
+    , ram_quota : Int
+    , ram_usage : Int
+    , net_limit : Resource
+    , cpu_limit : Resource
+    , total_resources : ResourceInEos
+    , self_delegated_bandwidth : Maybe ResourceInEos
+    , refund_request : Maybe Refund
+    }
+
+
+type alias ResourceInEos =
+    { net_weight : String
+    , cpu_weight : String
+    , ram_bytes : Maybe Int
     }
 
 
 type alias Resource =
-    { net_weight : String
-    , cpu_weight : String
-    , ram_bytes : Int
+    { used : Int
+    , available : Int
+    , max : Int
+    }
+
+
+type alias Refund =
+    { owner : String
+    , request_time : String
+    , net_amount : String
+    , cpu_amount : String
     }
 
 
@@ -47,12 +69,31 @@ initModel =
     , keyAccounts = []
     , account =
         { account_name = ""
-        , core_liquid_balance = Just "0 EOS"
+        , core_liquid_balance = "0 EOS"
+        , ram_quota = 0
+        , ram_usage = 0
+        , net_limit =
+            { used = 0, available = 0, max = 0 }
+        , cpu_limit =
+            { used = 0, available = 0, max = 0 }
         , total_resources =
             { net_weight = "0 EOS"
             , cpu_weight = "0 EOS"
-            , ram_bytes = 0
+            , ram_bytes = Just 0
             }
+        , self_delegated_bandwidth =
+            Just
+                { net_weight = "0 EOS"
+                , cpu_weight = "0 EOS"
+                , ram_bytes = Nothing
+                }
+        , refund_request =
+            Just
+                { owner = ""
+                , request_time = ""
+                , net_amount = ""
+                , cpu_amount = ""
+                }
         }
     , errMessage = ""
     }
@@ -111,7 +152,7 @@ update message model =
                                         [ ( "account_name", JE.string query ) ]
                                         |> Http.jsonBody
                             in
-                                post (apiUrl ++ "/v1/chain/get_account") body accountDecoder |> (Http.send OnFetchAccount)
+                                post (getFullPath "/v1/chain/get_account") body accountDecoder |> (Http.send OnFetchAccount)
 
                         Ok PublicKeyQuery ->
                             let
@@ -120,7 +161,7 @@ update message model =
                                         [ ( "public_key", JE.string query ) ]
                                         |> Http.jsonBody
                             in
-                                post (apiUrl ++ "/v1/history/get_key_accounts") body keyAccountsDecoder |> (Http.send OnFetchKeyAccounts)
+                                post (getFullPath "/v1/history/get_key_accounts") body keyAccountsDecoder |> (Http.send OnFetchKeyAccounts)
 
                         Err _ ->
                             Cmd.none
@@ -138,6 +179,11 @@ update message model =
 
         OnFetchKeyAccounts (Err error) ->
             ( { model | errMessage = "invalid length" }, Cmd.none )
+
+
+getFullPath : String -> String
+getFullPath path =
+    apiUrl ++ path
 
 
 parseQuery : String -> Result String Query
@@ -158,16 +204,46 @@ parseQuery query =
 
 accountDecoder : JD.Decoder Account
 accountDecoder =
-    JD.map3 Account
-        (JD.field "account_name" JD.string)
-        (JD.maybe <| JD.field "core_liquid_balance" JD.string)
-        (JD.field "total_resources"
-            (JD.map3 Resource
-                (JD.field "net_weight" JD.string)
-                (JD.field "cpu_weight" JD.string)
-                (JD.field "ram_bytes" JD.int)
+    decode Account
+        |> required "account_name" JD.string
+        |> optional "core_liquid_balance" JD.string "0 EOS"
+        |> required "ram_quota" JD.int
+        |> required "ram_usage" JD.int
+        |> required "net_limit"
+            (decode Resource
+                |> required "used" JD.int
+                |> required "available" JD.int
+                |> required "max" JD.int
             )
-        )
+        |> required "cpu_limit"
+            (decode Resource
+                |> required "used" JD.int
+                |> required "available" JD.int
+                |> required "max" JD.int
+            )
+        |> required "total_resources"
+            (decode ResourceInEos
+                |> required "net_weight" JD.string
+                |> required "cpu_weight" JD.string
+                |> required "ram_bytes" (JD.nullable JD.int)
+            )
+        |> required "self_delegated_bandwidth"
+            (JD.nullable
+                (decode ResourceInEos
+                    |> required "net_weight" JD.string
+                    |> required "cpu_weight" JD.string
+                    |> optional "ram_bytes" (JD.nullable JD.int) Nothing
+                )
+            )
+        |> required "refund_request"
+            (JD.nullable
+                (decode Refund
+                    |> required "owner" JD.string
+                    |> required "request_time" JD.string
+                    |> required "net_amount" JD.string
+                    |> required "cpu_amount" JD.string
+                )
+            )
 
 
 keyAccountsDecoder : JD.Decoder (List String)
@@ -195,6 +271,18 @@ post url body decoder =
 view : Model -> Html Message
 view model =
     div []
-        [ input [ placeholder "계정명, 퍼블릭키 검색하기", onInput InputSearch ] []
+        [ input [ placeholder "계정명, 퍼블릭키 검색하기", onInput InputSearch, onEnter (GetSearchResult (model.searchInput)) ] []
         , button [ onClick (GetSearchResult (model.searchInput)) ] [ text "검색하기" ]
         ]
+
+
+onEnter : Message -> Attribute Message
+onEnter msg =
+    let
+        isEnter code =
+            if code == 13 then
+                JD.succeed msg
+            else
+                JD.fail "not ENTER"
+    in
+        on "keydown" (JD.andThen isEnter keyCode)
