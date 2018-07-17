e808a0baf08202ee821effbff8ffeda5556c2a79
basakkang <redmayne@naver.com>
2018-07-17 16:54:49 +0900

    Separate Account Type and Decoder from Header.elm

--- app/frontend/src/elm/Header.elm
+++ app/frontend/src/elm/Header.elm
@@ -25,9 +25,9 @@ import Html.Attributes
 import Html.Events exposing (on, onInput, onClick, keyCode)
 import Http
 import Json.Decode as JD exposing (Decoder)
-import Json.Decode.Pipeline exposing (decode, required, optional)
 import Json.Encode as JE
 import Regex exposing (regex, contains)
+import Data.Account exposing (Account, ResourceInEos, Resource, Refund, accountDecoder, keyAccountsDecoder)
 
 
 -- MODEL
@@ -40,45 +40,6 @@ type alias Model =
     , keyAccounts : List String
     , account : Account
     , errMessage : String
-    }
-
-
-
--- TODO(boseok): add required fields
-
-
-type alias Account =
-    { account_name : String
-    , core_liquid_balance : String
-    , ram_quota : Int
-    , ram_usage : Int
-    , net_limit : Resource
-    , cpu_limit : Resource
-    , total_resources : ResourceInEos
-    , self_delegated_bandwidth : Maybe ResourceInEos
-    , refund_request : Maybe Refund
-    }
-
-
-type alias ResourceInEos =
-    { net_weight : String
-    , cpu_weight : String
-    , ram_bytes : Maybe Int
-    }
-
-
-type alias Resource =
-    { used : Int
-    , available : Int
-    , max : Int
-    }
-
-
-type alias Refund =
-    { owner : String
-    , request_time : String
-    , net_amount : String
-    , cpu_amount : String
     }
 
 
@@ -227,55 +188,6 @@ isAccount query =
 isPublicKey : String -> Bool
 isPublicKey query =
     contains (regex "^EOS[\\w]{50}$") query
-
-
-accountDecoder : JD.Decoder Account
-accountDecoder =
-    decode Account
-        |> required "account_name" JD.string
-        |> optional "core_liquid_balance" JD.string "0 EOS"
-        |> required "ram_quota" JD.int
-        |> required "ram_usage" JD.int
-        |> required "net_limit"
-            (decode Resource
-                |> required "used" JD.int
-                |> required "available" JD.int
-                |> required "max" JD.int
-            )
-        |> required "cpu_limit"
-            (decode Resource
-                |> required "used" JD.int
-                |> required "available" JD.int
-                |> required "max" JD.int
-            )
-        |> required "total_resources"
-            (decode ResourceInEos
-                |> required "net_weight" JD.string
-                |> required "cpu_weight" JD.string
-                |> required "ram_bytes" (JD.nullable JD.int)
-            )
-        |> required "self_delegated_bandwidth"
-            (JD.nullable
-                (decode ResourceInEos
-                    |> required "net_weight" JD.string
-                    |> required "cpu_weight" JD.string
-                    |> optional "ram_bytes" (JD.nullable JD.int) Nothing
-                )
-            )
-        |> required "refund_request"
-            (JD.nullable
-                (decode Refund
-                    |> required "owner" JD.string
-                    |> required "request_time" JD.string
-                    |> required "net_amount" JD.string
-                    |> required "cpu_amount" JD.string
-                )
-            )
-
-
-keyAccountsDecoder : JD.Decoder (List String)
-keyAccountsDecoder =
-    (JD.field "account_names" (JD.list JD.string))
 
 
 post : String -> Http.Body -> Decoder a -> Http.Request a
