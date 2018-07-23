ecc921be3883903638184655348df61c07d03b87
Sinhyeok Seo <sinhyeok@chain.partners>
2018-07-23 11:52:01 +0900

    Action mailer configuration for Gmail

--- config/environments/alpha.rb
+++ config/environments/alpha.rb
@@ -69,7 +69,17 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'chain.partners',
+    user_name:            ENV['EMAIL_USER'],
+    password:             ENV['EMAIL_PASS'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
   config.action_mailer.default_url_options = { host: 'ecs-first-run-alb-1125793223.ap-northeast-2.elb.amazonaws.com', port: 80 }
 
