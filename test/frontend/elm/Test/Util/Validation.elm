95b8e866dcab5b72fb43e005606b8d38496779ac
Sinhyeok Seo <sinhyeok@chain.partners>
2018-07-25 11:00:06 +0900

    Fix test error

---
+++ test/frontend/elm/Test/Util/Validation.elm
@@ -0,0 +1,53 @@
+module Test.Util.Validation exposing (..)
+
+import Expect
+import Navigation exposing (Location)
+import Util.Validation exposing (..)
+import Test exposing (..)
+
+
+location : Location
+location =
+    { href = ""
+    , host = ""
+    , hostname = ""
+    , protocol = ""
+    , origin = ""
+    , port_ = ""
+    , pathname = "/none"
+    , search = ""
+    , hash = ""
+    , username = ""
+    , password = ""
+    }
+
+
+tests : Test
+tests =
+    describe "Validation module"
+        [ describe "isAccount"
+            [ test "a-z.1-5 True" <|
+                \() ->
+                    Expect.equal (True) (isAccount "abc.xyz12345")
+            , test "a-z.1-5 False" <|
+                \() ->
+                    Expect.equal (False) (isAccount "ABC!@#$67890")
+            , test "{1,12} True" <|
+                \() ->
+                    Expect.equal (True) (isAccount "eosio.ram")
+            , test "{1,12} False" <|
+                \() ->
+                    Expect.equal (False) (isAccount "eosio.ram.eosio.system")
+            ]
+        , describe "isPublicKey"
+            [ test "start with EOS True" <|
+                \() ->
+                    Expect.equal (True) (isPublicKey "EOS5uxjV3FYZvwqyAM2StkFEvUvf43F7gSrZcBpunuuTxiYkKqb6d")
+            , test "start with EOS False" <|
+                \() ->
+                    Expect.equal (False) (isPublicKey "eos5uxjV3FYZvwqyAM2StkFEvUvf43F7gSrZcBpunuuTxiYkKqb6d")
+            , test "length 53 False" <|
+                \() ->
+                    Expect.equal (False) (isAccount "EOS5uxjV3FYZvwqyAM2StkFEvUvf43F7")
+            ]
+        ]
