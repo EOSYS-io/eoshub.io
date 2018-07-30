1775cd39d65408f5aba3ba6d1d1ac19ce3a589c6
HeejaeJoo <ymount02@gmail.com>
2018-07-30 10:54:18 +0900

    Move mainnet url from HttpRequest to Urls.

--- test/frontend/elm/Test/Util/HttpRequest.elm
+++ test/frontend/elm/Test/Util/HttpRequest.elm
@@ -3,6 +3,7 @@ module Test.Util.HttpRequest exposing (..)
 import Util.HttpRequest exposing (..)
 import Expect
 import Test exposing (..)
+import Util.Urls exposing (mainnetRpcUrl)
 
 
 tests : Test
@@ -15,9 +16,9 @@ tests =
             [ describe "getFullPath"
                 [ test "get_account" <|
                     \() ->
-                        Expect.equal (apiUrl ++ "/v1/chain/get_account") (getFullPath "/v1/chain/get_account")
+                        Expect.equal (mainnetRpcUrl ++ "/v1/chain/get_account") (getFullPath "/v1/chain/get_account")
                 , test "get_key_accounts" <|
                     \() ->
-                        Expect.equal (apiUrl ++ "/v1/history/get_key_accounts") (getFullPath "/v1/history/get_key_accounts")
+                        Expect.equal (mainnetRpcUrl ++ "/v1/history/get_key_accounts") (getFullPath "/v1/history/get_key_accounts")
                 ]
             ]
