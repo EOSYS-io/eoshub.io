9ee4c9bcdfb49d31ce855935106f77930dbe9266
HeejaeJoo <ymount02@gmail.com>
2018-07-30 14:05:41 +0900

    Move eosio get account response handling logic to Data/Account.

--- app/frontend/src/elm/Component/Main/Page/Search.elm
+++ app/frontend/src/elm/Component/Main/Page/Search.elm
@@ -38,10 +38,24 @@ import Http
 import Util.HttpRequest exposing (getFullPath, post)
 import Json.Encode as Encode
 import Data.Action exposing (Action)
-import Data.Account exposing (Account, ResourceInEos, Resource, Refund, accountDecoder, keyAccountsDecoder)
-import Util.Formatter exposing (larimerToEos, eosFloatToString, eosStringToFloat, unitConverterRound4, percentageConverter)
-import Util.Constant exposing (second, minute, hour, day, kilo, mega, giga, tera)
-import Round
+import Data.Account
+    exposing
+        ( Account
+        , ResourceInEos
+        , Resource
+        , Refund
+        , accountDecoder
+        , defaultAccount
+        , keyAccountsDecoder
+        , getTotalAmount
+        , getUnstakingAmount
+        , getResource
+        )
+import Util.Formatter
+    exposing
+        ( larimerToEos
+        , eosFloatToString
+        )
 
 
 -- MODEL
@@ -55,34 +69,7 @@ type alias Model =
 
 initModel : Model
 initModel =
-    { account =
-        { account_name = "Loading..."
-        , core_liquid_balance = "0 EOS"
-        , voter_info =
-            { staked = 0 }
-        , ram_quota = 0
-        , ram_usage = 0
-        , net_limit =
-            { used = 0, available = 0, max = 0 }
-        , cpu_limit =
-            { used = 0, available = 0, max = 0 }
-        , total_resources =
-            { net_weight = "0 EOS"
-            , cpu_weight = "0 EOS"
-            , ram_bytes = Just 0
-            }
-        , self_delegated_bandwidth =
-            { net_weight = "0 EOS"
-            , cpu_weight = "0 EOS"
-            , ram_bytes = Nothing
-            }
-        , refund_request =
-            { owner = ""
-            , request_time = ""
-            , net_amount = "0 EOS"
-            , cpu_amount = "0 EOS"
-            }
-        }
+    { account = defaultAccount
     , actions = []
     }
 
@@ -331,116 +318,3 @@ view language { account } =
                     ]
                 ]
             ]
-
-
-
--- Total Amount = core_liquid_balance + (staked * 0.0001) + (unstaking_net_amount + unstaking_cpu_amount)
-
-
-getTotalAmount : String -> Int -> String -> String -> String
-getTotalAmount core_liquid_balance staked unstaking_net_amount unstaking_cpu_amount =
-    (eosStringToFloat (core_liquid_balance)
-        + (larimerToEos staked)
-        + eosStringToFloat (unstaking_net_amount)
-        + eosStringToFloat (unstaking_cpu_amount)
-        |> Round.round 4
-    )
-        ++ " EOS"
-
-
-getUnstakingAmount : String -> String -> String
-getUnstakingAmount unstaking_net_amount unstaking_cpu_amount =
-    eosFloatToString (eosStringToFloat unstaking_net_amount + eosStringToFloat unstaking_cpu_amount)
-
-
-
--- getResource ( cpu/net, used, available, max ) -> ( totalString, avaliablePercent )
-
-
-getResource : String -> Int -> Int -> Int -> ( String, String, String )
-getResource resourceType used available max =
-    let
-        totalString =
-            case resourceType of
-                "net" ->
-                    -- Bytes
-                    if max < kilo then
-                        (toString max) ++ " bytes"
-                        -- KB
-                    else if (max >= kilo) && (max < mega) then
-                        unitConverterRound4 max kilo ++ " KB"
-                        -- MB
-                    else if (max >= mega) && (max < giga) then
-                        unitConverterRound4 max mega ++ " MB"
-                        -- GB
-                    else if (max >= giga) && (max < tera) then
-                        unitConverterRound4 max giga ++ " GB"
-                        -- TB
-                    else
-                        unitConverterRound4 max tera ++ " TB"
-
-                "cpu" ->
-                    -- ms
-                    if max < second then
-                        (toString max) ++ " ms"
-                        -- second
-                    else if (max >= second) && (max < minute) then
-                        unitConverterRound4 max second ++ " s"
-                        -- minute
-                    else if (max >= minute) && (max < hour) then
-                        unitConverterRound4 max minute ++ " min"
-                        -- hour
-                    else if (max >= hour) && (max < day) then
-                        unitConverterRound4 max hour ++ " hour"
-                        -- day
-                    else
-                        unitConverterRound4 max day ++ " day"
-
-                "ram" ->
-                    -- Bytes
-                    if max < 1024 then
-                        (toString max) ++ " bytes"
-                        -- KB
-                    else if (max >= kilo) && (max < mega) then
-                        unitConverterRound4 max kilo ++ " KB"
-                        -- MB
-                    else if (max >= mega) && (max < giga) then
-                        unitConverterRound4 max mega ++ " MB"
-                        -- GB
-                    else if (max >= giga) && (max < tera) then
-                        unitConverterRound4 max giga ++ " GB"
-                        -- TB
-                    else
-                        unitConverterRound4 max tera ++ " TB"
-
-                _ ->
-                    ""
-
-        avaliablePercent =
-            case max of
-                0 ->
-                    "0%"
-
-                _ ->
-                    (percentageConverter available max |> Round.round 2) ++ "%"
-
-        color =
-            case max of
-                0 ->
-                    "hell"
-
-                _ ->
-                    let
-                        percentage =
-                            percentageConverter available max
-                    in
-                        if percentage < 25 then
-                            "hell"
-                        else if (percentage >= 25 && percentage < 50) then
-                            "bad"
-                        else if (percentage >= 50 && percentage < 75) then
-                            "good"
-                        else
-                            "fine"
-    in
-        ( totalString, avaliablePercent, color )
