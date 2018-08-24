ea7473607223f4cffb28cbb57ac5882ef140a516
HeejaeJoo <ymount02@gmail.com>
2018-08-24 16:06:37 +0900

    Implement index layout.

--- app/frontend/src/elm/Component/Main/Page/Index.elm
+++ app/frontend/src/elm/Component/Main/Page/Index.elm
@@ -19,13 +19,57 @@ type Message
 
 view : Language -> Html Message
 view language =
-    section [ class "action view panel" ]
-        [ a
-            [ onClick (ChangeUrl "/transfer")
-            ]
-            [ div [ class "card transfer" ]
-                [ h3 [] [ text (translate language Transfer) ]
-                , p [] [ text (translate language TransferDesc) ]
+    main_ [ class "index" ]
+        [ section [ class "menu_area" ]
+            [ h2 [] [ text "Menu" ]
+            , div [ class "container" ]
+                [ div [ class "greeting" ]
+                    [ h3 []
+                        [ text (translate language Hello)
+                        , br [] []
+                        , text (translate language WelcomeEosHub)
+                        ]
+                    , p
+                        []
+                        [ a [] [ text "이오스 허브 사용법 보기" ]
+                        ]
+                    ]
+                , a
+                    [ onClick (ChangeUrl "/resource")
+                    , class "card resource"
+                    ]
+                    [ h3 [] [ text "리소스 관리" ]
+                    , p [] [ text "이오스 동작을 위한 리소스를 관리합니다." ]
+                    ]
+                , a
+                    [ onClick (ChangeUrl "/transfer")
+                    , class "card transfer"
+                    ]
+                    [ h3 [] [ text (translate language Transfer) ]
+                    , p [] [ text (translate language TransferDesc) ]
+                    ]
+                , a
+                    [ onClick (ChangeUrl "/ram-market")
+                    , class "card ram_market"
+                    ]
+                    [ h3 [] [ text (translate language RamMarket) ]
+                    , p [] [ text (translate language RamMarketDesc) ]
+                    ]
+                , a
+                    [ onClick (ChangeUrl "/voting")
+                    , class "card vote"
+                    ]
+                    [ h3 [] [ text (translate language Vote) ]
+                    , p [] [ text (translate language VoteDesc) ]
+                    ]
                 ]
+            ]
+        , section [ class "dapps" ]
+            [ h2 [] [ text "최신 이오스 디앱" ]
+            , p [] [ text "업데이트되는 다양한 이오스 디앱들을 만나보세요" ]
+            , div [ class "rolling banner" ]
+                [ text "Comming Soon"
+                ]
+            , a [ class "view more" ] [ text " 더 보기" ]
             ]
         ]
