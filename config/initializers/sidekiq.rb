1dea74a107fc003df0ba418f24c825f05515a8f2
HeejaeJoo <ymount02@gmail.com>
2018-09-03 16:54:45 +0900

    Change sidekiq enque timing.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,8 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: Rails.configuration.urls['redis_host'] }
+  config.on(:startup) do
+    job = Sidekiq::Cron::Job.find('cron_job').enque!
+  end
 end
 
 Sidekiq.configure_client do |config|
@@ -11,5 +14,3 @@ schedule_file = "config/schedule.yml"
 if File.exist?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
-
-job = Sidekiq::Cron::Job.find('cron_job').enque!
