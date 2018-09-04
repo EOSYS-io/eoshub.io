38581ab5f9771da0974001baf126c7aa633c9456
HeejaeJoo <ymount02@gmail.com>
2018-09-04 17:01:12 +0900

    Modify table comment.

--- app/models/eos_ram_price_history.rb
+++ app/models/eos_ram_price_history.rb
@@ -2,12 +2,12 @@
 #
 # Table name: eos_ram_price_histories
 #
-#  close      :decimal(, )      not null
+#  close      :decimal(38, 8)   not null
 #  end_time   :datetime         not null
-#  high       :decimal(, )      not null
+#  high       :decimal(38, 8)   not null
 #  intvl      :integer          not null
-#  low        :decimal(, )      not null
-#  open       :decimal(, )      not null
+#  low        :decimal(38, 8)   not null
+#  open       :decimal(38, 8)   not null
 #  start_time :datetime         not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
