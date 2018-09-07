0c1d7d948b467e817720df880c1eca1bf27afc7b
HeejaeJoo <ymount02@gmail.com>
2018-09-07 16:01:41 +0900

    Move charting library, and add static settings.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,8 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
 const elm =  require('./loaders/elm')
 require('dotenv').config();
 
 environment.loaders.append('elm', elm)
+
 module.exports = environment
