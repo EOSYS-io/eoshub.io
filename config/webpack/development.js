68662db92e0a7c504f83aa01d9c0658c9e181d87
HeejaeJoo <ymount02@gmail.com>
2018-09-11 13:54:18 +0900

    Move write-file-plugin to devlopment.

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,11 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+const WriteFilePlugin = require('write-file-webpack-plugin')
 
 const environment = require('./environment')
+
+environment.plugins.append(
+  'WriteFilePlugin',
+  new WriteFilePlugin()
+);
 
 module.exports = environment.toWebpackConfig()
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,6 @@
 const { environment } = require('@rails/webpacker')
 const elm =  require('./loaders/elm')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-const WriteFilePlugin = require('write-file-webpack-plugin')
 require('dotenv').config();
 
 environment.loaders.append('elm', elm)
@@ -15,11 +14,6 @@ environment.plugins.append(
       context: 'vendor/assets/charting_library',
     }
   ])
-);
-
-environment.plugins.append(
-  'WriteFilePlugin',
-  new WriteFilePlugin()
 );
 
 module.exports = environment
