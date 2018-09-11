e1f1e9a105c0a74adc250f24a3c219d3a3b54801
HeejaeJoo <ymount02@gmail.com>
2018-09-11 13:58:42 +0900

    Call create with array.

--- db/migrate/20180910110901_add_initial_price_history_intvls.rb
+++ db/migrate/20180910110901_add_initial_price_history_intvls.rb
@@ -2,12 +2,18 @@ class AddInitialPriceHistoryIntvls < ActiveRecord::Migration[5.2]
   def up
     # 1m, 3m, 5m, 15m, 30m, 1h, 4h, 1d, 1w.
     price_intvl_list_as_seconds = [
-      60, 180, 300, 900, 1800, 3600, 14400, 86400, 604800
+      { seconds: 60 },
+      { seconds: 180 },
+      { seconds: 300 },
+      { seconds: 900 },
+      { seconds: 1800 },
+      { seconds: 3600 },
+      { seconds: 14400 },
+      { seconds: 86400 },
+      { seconds: 604800 }
     ]
 
-    price_intvl_list_as_seconds.each do | intvl | 
-      PriceHistoryIntvl.create(seconds: intvl)
-    end
+    PriceHistoryIntvl.create(price_intvl_list_as_seconds)
   end
 
   def down
