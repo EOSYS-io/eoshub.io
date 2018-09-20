0e71ea520c4101883e4387799ccdb30ddad1f998
Sinhyeok Seo <sinhyeok@chain.partners>
2018-09-20 11:07:24 +0900

    Fix test according to latest controller change

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class OrdersControllerTest < ActionController::TestCase
+  fixtures :products
+
   test "should post request_payment" do
     class MockResponse 
       def initialize(mock_body)
@@ -16,10 +18,12 @@ class OrdersControllerTest < ActionController::TestCase
       end
     end
 
+    product = products(:one)
+
     @controller.stub :eos_account_exist?, false do
       mock_body = file_fixture('payletter_payment_request_response_success.json').read
       Typhoeus::Request.stub_any_instance :run, MockResponse.new(mock_body) do
-        post :request_payment, body: { pgcode: 'virtualaccount', eos_account: 'testtesttest', amount: 1000, product_name: 'eos_account' }.to_json, as: :json
+        post :request_payment, body: { pgcode: 'virtualaccount', eos_account: 'testtesttest', product_id: product.id }.to_json, as: :json
         assert_response :ok
       end
     end
