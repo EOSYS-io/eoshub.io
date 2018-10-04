9b4699bc6e708d2cbb3676c77661f563a9306b29
HeejaeJoo <ymount02@gmail.com>
2018-10-04 19:27:38 +0900

    Add bang to sidekiq cron load_from_hash and rename jobs.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,8 +3,7 @@ Sidekiq.configure_server do |config|
   config.on(:startup) do
     schedule_file = "config/schedule.yml"
     if File.exist?(schedule_file) && Sidekiq.server?
-      Sidekiq.redis { |conn| conn.flushdb  }
-      Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+      Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)
     end
   end
 end
