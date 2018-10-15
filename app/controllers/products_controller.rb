d0ba6242a8871da3f414773ec4cd71ccf599abb4
Sinhyeok Seo <sinhyeok@chain.partners>
2018-10-15 18:25:12 +0900

    Change eos account product name

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def eos_account
-    product = Product.where(name: 'eos_account').where(active: true).take
+    product = Product.where(name: 'EOS Account').where(active: true).take
     raise Exceptions::DefaultError, Exceptions::DEACTIVATED_PRODUCT if product.blank?
 
     render json: product, status: :ok
