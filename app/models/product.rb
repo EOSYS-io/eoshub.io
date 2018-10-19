d6073dff516d3af5d257d23e7c88589ed301504d
Sinhyeok Seo <sinhyeok@chain.partners>
2018-10-19 10:40:59 +0900

    Add event_activation to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,12 +2,13 @@
 #
 # Table name: products
 #
-#  id         :bigint(8)        not null, primary key
-#  active     :boolean          default(FALSE), not null
-#  name       :string           not null
-#  price      :integer          not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id               :bigint(8)        not null, primary key
+#  active           :boolean          default(FALSE), not null
+#  event_activation :boolean          default(FALSE), not null
+#  name             :string           not null
+#  price            :integer          not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
 #
 # Indexes
 #
@@ -18,8 +19,9 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true
   validates :active, inclusion: { in: [true, false] }
+  validates :event_activation, inclusion: { in: [true, false] }
 
   def as_json(*args)
-    { id: id, name: name, price: price }
+    { id: id, name: name, price: price, event_activation: event_activation }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_18_030038) do
+ActiveRecord::Schema.define(version: 2018_10_19_013513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -129,6 +129,7 @@ ActiveRecord::Schema.define(version: 2018_10_18_030038) do
     t.boolean "active", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "event_activation", default: false, null: false
     t.index ["name"], name: "index_products_on_name"
   end
 
