2b6b6c6a8c520a877b5409673a60dde971f43fed
Sinhyeok Seo <sinhyeok@chain.partners>
2018-10-22 14:23:01 +0900

    Show resouces info from product api response

--- db/migrate/20181022013039_add_cpu_net_ram_field_to_product.rb
+++ db/migrate/20181022013039_add_cpu_net_ram_field_to_product.rb
@@ -3,5 +3,7 @@ class AddCpuNetRamFieldToProduct < ActiveRecord::Migration[5.2]
     add_column :products, :cpu, :float, default: 0
     add_column :products, :net, :float, default: 0
     add_column :products, :ram, :integer, default: 0
+
+    Product.eos_account.update(cpu: 0.1, net: 0.01, ram: 3072)
   end
 end
