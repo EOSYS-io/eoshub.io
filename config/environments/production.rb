45826b97546d9f0b18e9576948ddd1d91dc86fc4
Sinhyeok Seo <sinhyeok@chain.partners>
2018-10-22 15:07:29 +0900

    Add protocol option for devise confirmation mail link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
     authentication:       'plain',
     enable_starttls_auto: true
   }
-  config.action_mailer.default_url_options = { host: 'eoshub.io', port: 443 }
+  config.action_mailer.default_url_options = { protocol: 'https', :host => 'eoshub.io' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
