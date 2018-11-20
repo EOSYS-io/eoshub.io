dc2883fafdf14ac32b38ae96fcf522726f940d05
basakkang <redmayne@naver.com>
2018-11-20 15:16:03 +0900

    Add event_activation in ApplicationState

--- app/controllers/application_states_controller.rb
+++ app/controllers/application_states_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationStatesController < ApplicationController
+  include EosAccount
+
   def index
     announcement = Announcement.where('active = true AND published_at <= NOW() AND ended_at >= NOW()').order("published_at DESC").take;
-    setting= Setting.order("created_at DESC").take;
-    render json: {status: 'SUCCESS', data:{announcement:announcement, setting:setting}}, status: :ok
+    setting = Setting.order("created_at DESC").take;
+    product = Product.eos_account
+    event_activation = product.event_activation
+    
+    render json: {status: 'SUCCESS', data:{announcement:announcement, setting:setting, event_activation:event_activation}}, status: :ok
   end
 end
