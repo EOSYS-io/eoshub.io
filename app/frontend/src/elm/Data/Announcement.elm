49a73282a0bcd563d1ef41c6af369cf32b3e0a3c
basakkang <redmayne@naver.com>
2018-11-20 16:31:05 +0900

    Add Data.Announcement

---
+++ app/frontend/src/elm/Data/Announcement.elm
@@ -0,0 +1,48 @@
+module Data.Announcement exposing (Announcement, initAnnouncement)
+
+import Json.Decode as Decode exposing (Decoder)
+import Json.Decode.Pipeline exposing (decode, required)
+
+
+type alias Announcement =
+    { id : Int
+    , titleKo : String
+    , titleEn : String
+    , titleCn : String
+    , active : Bool
+    , bodyKo : String
+    , bodyEn : String
+    , bodyCn : String
+    , publishedAt : String
+    , endedAt : String
+    }
+
+
+initAnnouncement : Announcement
+initAnnouncement =
+    { id = 0
+    , titleKo = ""
+    , titleEn = ""
+    , titleCn = ""
+    , active = False
+    , bodyKo = ""
+    , bodyEn = ""
+    , bodyCn = ""
+    , publishedAt = ""
+    , endedAt = ""
+    }
+
+
+announcementDecoder : Decoder Announcement
+announcementDecoder =
+    decode Announcement
+        |> required "id" Decode.int
+        |> required "title_ko" Decode.string
+        |> required "title_en" Decode.string
+        |> required "title_cn" Decode.string
+        |> required "active" Decode.bool
+        |> required "body_ko" Decode.string
+        |> required "body_en" Decode.string
+        |> required "body_cn" Decode.string
+        |> required "published_at" Decode.string
+        |> required "ended_at" Decode.string
